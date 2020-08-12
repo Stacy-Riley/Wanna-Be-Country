caee5158791071aafb65f08680756392998425a7
stacy_r <stacyrileyca@gmail.com>
2020-08-12 17:01:20 -0400

    Added text edit code to creat-posts.html page

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,19 @@ function validateForm() {
 //  txt = "User cancelled the prompt.";
 //  txt = "Hello " + person + "! How are you today?";
 //
+
+
+ClassicEditor
+    .create( document.querySelector( '#body' ), {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    } )
+    .catch( error => {
+        console.log( error );
+    } );
